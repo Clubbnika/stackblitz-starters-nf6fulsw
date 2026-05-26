ccda4fcd6ca138cec58cfb029af9c6940a243ac1
Clubbnika <veronika.oxt12@gmail.com>
2026-05-26 23:55:27 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-      // Це дозволить успішно завершити деплой, навіть якщо в коді є дрібні помилки лінтера
+      // Ігноруємо помилки лінтера (лапки, апострофи) при деплої
       ignoreDuringBuilds: true,
     },
     typescript: {
-      // На всякий випадок ігноруємо і дрібні варнінги типів при збірці
+      // Ігноруємо дрібні варнінги типів при збірці
       ignoreBuildErrors: true,
     }
   }
   
-  module.exports = nexTConfig
+  module.exports = nextConfig
